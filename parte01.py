7e2418fade07a6784a23821f575ac87e8d5455d5
Vitor MeirelesP <pintorgamer12@gmail.com>
2023-11-30 16:44:41 -0300

    parte inicial do jogo

---
+++ parte01.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+
+janela = pygame.display.set_mode((800,600))
+pygame.display.set_caption('Jogo do Vitor Meireles')
+
+janela_aberta = True
+while janela_aberta:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            janela_aberta == False
+
+pygame.quit()
